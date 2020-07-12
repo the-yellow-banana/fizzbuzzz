12d80967aaed4556607d414049175503a77f1e3c
the-yellow-banana <67622759+the-yellow-banana@users.noreply.github.com>
2020-07-12 17:53:17 -0400

    Update scratch.py

--- scratch.py
+++ scratch.py
@@ -1,12 +1,16 @@
 import numpy
+
+firstnum= int(input("Put your first number to check for here:"))
+secondnum= int(input("Put your second number to check for here:"))
 randomnums= numpy.random.randint(1, 1000, 100)
+
 def fizzbuzz(some_nums):
     for i in some_nums:
-            if i%3==0 and i%5==0:
+            if i%firstnum==0 and i%secondnum==0:
                 print("fizzbuzz")
-            elif i%3==0:
+            elif i%firstnum==0:
                 print("fizz")
-            elif i%5==0:
+            elif i%secondnum==0:
                 print("buzz")
             else:
                 print(i)
